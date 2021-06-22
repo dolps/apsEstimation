c75944eb9ddac46d899e4ebe02ecd74d4aeeecf1
Thomas Dølplads <thomas.dolplads@drift.oslo.kommune.no>
2021-06-23 00:56:15 +0200

    fix error

--- src/components/shared/CreationForm.js
+++ src/components/shared/CreationForm.js
@@ -2,15 +2,14 @@ import React from "react";
 import {useForm} from "react-hook-form";
 import {FormControl, Input, Button, Alert, AlertIcon} from "@chakra-ui/react"
 
-const CreationForm = ({submission,placeholder,submitBtnTxt}) => {
-    const {handleSubmit, register, setError, formState: {errors, isSubmitting},reset} = useForm();
+const CreationForm = ({submission, placeholder, submitBtnTxt}) => {
+    const {handleSubmit, register, setError, formState: {errors, isSubmitting}, reset} = useForm();
 
     const onSubmit = async (data) => {
         try {
-            submission(data).then(()=>{
+            submission(data).then(() => {
                 reset();
             });
-
         } catch (error) {
             setError('input', {
                 type: 'manual',
@@ -19,17 +18,17 @@ const CreationForm = ({submission,placeholder,submitBtnTxt}) => {
         }
     };
 
-    return(
+    return (
         <>
             {errors?.input && (<Alert status="error" variant="subtle" mt={6} mb={6}>
                 <AlertIcon/>
-                {errors.input.message}
+                {errors.input.type === 'required' ? 'required field is missing' : errors.input.message}
             </Alert>)}
             <form onSubmit={handleSubmit(onSubmit)}>
                 <FormControl>
                     <Input name="input"
                            fontSize="sm"
-                           placeholder={placeholder}  {...register("input", {})}
+                           placeholder={placeholder}  {...register("input", {required: true})}
                            type="text"/>
                 </FormControl>
                 <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">{submitBtnTxt}</Button>
