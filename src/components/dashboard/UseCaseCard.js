4b7826382a35a8c702eb6bd25f288cc88fc1f6da
Thomas Dølplads <thomas.dolplads@drift.oslo.kommune.no>
2021-06-22 14:43:31 +0200

    calculations now works

--- src/components/dashboard/UseCaseCard.js
+++ src/components/dashboard/UseCaseCard.js
@@ -1,8 +1,10 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
 import {
     Box,
     Text,
     Stack,
+    HStack,
+    VStack,
     IconButton,
     NumberInput,
     NumberInputField,
@@ -16,9 +18,11 @@ import {
 
 import {MdDelete} from "react-icons/md";
 import {fireStore} from "../../hooks/useAuth";
+import {EstimationContext} from "../../context/EstimationContext";
 
 // TODO https://codesandbox.io/s/affectionate-swartz-9yk2u?file=/src/App.js:168-222 debounce
 function UseCaseCard(props) {
+    const estimationContext = useContext(EstimationContext);
     const {usecase, id, title, summary, longLine, onRemove} = props;
     const collection = fireStore.collection(`projects/${usecase.projectId}/usecases`);
     const format = (val) => val + `h`
@@ -34,8 +38,55 @@ function UseCaseCard(props) {
     };
 
     const onInsecurityGradeChange = async (event) => {
+        console.log('updating insecuritygrade: ' + event.target.value);
         setInsecurityGrade(event.target.value);
         await collection.doc(id).update('insecurityGrade', event.target.value);
+    };
+
+    const getInsecurityFactor = () => {
+        switch (Number(usecase.insecurityGrade)) {
+            case 1:
+                return {mostLikely: 1.3, worst: 1.8};
+            case 2:
+                return {mostLikely: 1.4, worst: 2.9};
+            case 3:
+                return {mostLikely: 2, worst: 6};
+            default:
+                throw new Error('Not a valid case: ');
+
+        }
+    };
+
+    const calculateMostLikely = () => {
+        const res = calculateBest() * getInsecurityFactor().mostLikely; // 2 == Høy usikkerhet for mostlikely
+        return parseFloat(res.toFixed(1))
+    };
+
+    const calculateBest = () => {
+        getInsecurityFactor();
+        let use = parseInt(developmentTime);
+        const designTime = (use * estimationContext.designAddon) / 100;
+        const testTime = (use * estimationContext.testAddon) / 100;
+        const totalDevelopmentTime = use + designTime + testTime;
+        const mgmtTime = (totalDevelopmentTime * estimationContext.MGMTAddon) / 100;
+
+        const result = use + designTime + testTime + mgmtTime;
+
+        return parseFloat(result.toFixed(1));
+    };
+
+    const calculateWorst = () => {
+        const res = calculateBest() * getInsecurityFactor().worst; // 6 == Høy usikkerhet for worst
+        return parseFloat(res.toFixed(1))
+    };
+
+    const calculateExpected = () => {
+        const best = calculateBest();
+        const mostLikely = calculateMostLikely() * 4;
+        const worst = calculateWorst();
+
+        const res = ((best + mostLikely + worst) / 6).toFixed(1);
+        return parseFloat(res);
     };
 
     return (
@@ -115,13 +166,16 @@ function UseCaseCard(props) {
                     calculated completion times:
                 </Text>
                 <Text my={2} color="gray.500">
-                    expected: {longLine}h
+                    best:{calculateBest()}h
                 </Text>
                 <Text my={2} color="gray.500">
-                    best:{longLine}h
+                    most likely:{calculateMostLikely()}h
                 </Text>
                 <Text my={2} color="gray.500">
-                    worst: {longLine}h
+                    expected: {calculateExpected()}h
+                </Text>
+                <Text my={2} color="gray.500">
+                    worst: {calculateWorst()}h
                 </Text>
             </Stack>
 
